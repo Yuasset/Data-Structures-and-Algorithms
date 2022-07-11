4b9456ccf685cc89d5d94b9fb7bd4be9c8acb625
Yuşa Çağatay GÜNAYDIN <44982368+Yuasset@users.noreply.github.com>
2022-07-11 16:27:44 +0300

    Update insertionSort.java

--- Insertion Sort/insertionSort.java
+++ Insertion Sort/insertionSort.java
@@ -2,7 +2,7 @@ public class insertionSort
 {
 	public static void main(String[] args)
 	{
-		int array[] = {6,2,66,33,8,12,52,11,4};
+		int array[] = {6,2,66,33,8,12,52,11,5};
 		System.out.print("Normal Array\t: ");
 		for (int count : array) System.out.print(count + " ");
 		sortingClass sorting = new sortingClass();
